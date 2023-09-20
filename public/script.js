323d09d84eaa36865ad54e2974a835d37c1d32c7
Uglješa Lukešević <84191191+ukicomputers@users.noreply.github.com>
2023-09-20 20:57:25 +0200

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,179 +1,67 @@
-//First the variables our app is going to use need to be declared
+class scrollPages {
+    constructor(container) {
+        this.container = container;
 
-//References to DOM elements
-var $window = $(window);
-var $document = $(document);
-//Only links that starts with #
-var $navButtons = $("nav a").filter("[href^=#]");
-var $navGoPrev = $(".go-prev");
-var $navGoNext = $(".go-next");
-var $slidesContainer = $(".slides-container");
-var $slides = $(".slide");
-var $currentSlide = $slides.first();
+        this.container.addEventListener("wheel", ({ wheelDelta }) => {
+            if (wheelDelta < 0) this.openNext();
+            else this.openPrev();
+        });
 
-//Animating flag - is our app animating
-var isAnimating = false;
+        this.hammer = new Hammer(this.container);
+        this.hammer.get("swipe").set({ direction: Hammer.DIRECTION_VERTICAL });
+        this.hammer.on("swipeup", () => this.openNext());
+        this.hammer.on("swipedown", () => this.openPrev());
 
-//The height of the window
-var pageHeight = $window.innerHeight();
+        window.addEventListener("keydown", ({ key }) => {
+            if (key === "ArrowDown") this.openNext();
+            if (key === "ArrowUp") this.openPrev();
+        });
 
-//Key codes for up and down arrows on keyboard. We'll be using this to navigate change slides using the keyboard
-var keyCodes = {
-    UP: 38,
-    DOWN: 40
+        this.updateView();
+    }
+
+    _currentPageIndex = 0;
+
+    get currentPageIndex() {
+        return this._currentPageIndex;
+    }
+
+    set currentPageIndex(index) {
+        this._currentPageIndex = index;
+        this.updateView();
+    }
+
+    get pages() {
+        return [...this.container.querySelectorAll(".hero"), ...this.container.querySelectorAll(".dpPage"), ...this.container.querySelectorAll(".mp"), ...this.container.querySelectorAll(".bbl")].map(elem => ({ elem }));
+    }
+
+    openNext() {
+        let nextIndex = this.currentPageIndex + 1;
+        if (nextIndex <= this.pages.length - 1) this.currentPageIndex = nextIndex;
+    }
+
+    openPrev() {
+        let prevIndex = this.currentPageIndex - 1;
+        if (prevIndex >= 0) this.currentPageIndex = prevIndex;
+    }
+
+    updateView() {
+        this.pages.forEach(page => page.elem.classList.remove("open"));
+        this.pages[this.currentPageIndex].elem.classList.add("open");
+        console.log(this.currentPageIndex);
+        if (this.currentPageIndex == 1) {
+            this.container.querySelector(".dpPage .wystu").style.animation = "fadeIn 2s";
+            this.container.querySelector(".dpPage .info").style.animation = "niceLeft 2s";
+            this.container.querySelector(".dpPage img").style.animation = "niceImg 2s forwards";
+        } else if (this.currentPageIndex == 2) {
+            this.container.querySelector(".mp .wystu").style.animation = "fadeIn 2s";
+            this.container.querySelector(".mp .info .dmp").style.animation = "dmpAnim 2s forwards";
+            this.container.querySelector(".mp .info .inf").style.animation = "infAnim 3s forwards";
+            this.container.querySelector(".mp .info img").style.animation = "vidTrans 3s forwards";
+        }
+    }
 }
 
-//Going to the first slide
-goToSlide($currentSlide);
-
-
-/*
-*   Adding event listeners
-* */
-
-$window.on("resize", onResize).resize();
-$window.on("mousewheel DOMMouseScroll", onMouseWheel);
-$document.on("keydown", onKeyDown);
-$navButtons.on("click", onNavButtonClick);
-$navGoPrev.on("click", goToPrevSlide);
-$navGoNext.on("click", goToNextSlide);
-var self = this;
-$document.touchwipe({
-    wipeUp: function () {
-        self.goToNextSlide();
-    },
-    wipeDown: function () {
-        self.goToPrevSlide();
-    },
-    min_move_x: 50,
-    min_move_y: 15,
-    preventDefaultEvents: true
+document.addEventListener("DOMContentLoaded", () => {
+    new scrollPages(document.querySelector(".scrollPages"));
 });
-/*
-*   Internal functions
-* */
-
-
-/*
-*   When a button is clicked - first get the button href, and then slide to the container, if there's such a container
-* */
-function onNavButtonClick(event) {
-    //The clicked button
-    var $button = $(this);
-
-    //The slide the button points to
-    var $slide = $($button.attr("href"));
-
-    //If the slide exists, we go to it
-    if ($slide.length) {
-        goToSlide($slide);
-        event.preventDefault();
-    }
-}
-
-/*
-*   Getting the pressed key. Only if it's up or down arrow, we go to prev or next slide and prevent default behaviour
-*   This way, if there's text input, the user is still able to fill it
-* */
-function onKeyDown(event) {
-
-    var PRESSED_KEY = event.keyCode;
-
-    if (PRESSED_KEY == keyCodes.UP) {
-        goToPrevSlide();
-        event.preventDefault();
-    }
-    else if (PRESSED_KEY == keyCodes.DOWN) {
-        goToNextSlide();
-        event.preventDefault();
-    }
-
-}
-
-/*
-*   When user scrolls with the mouse, we have to change slides
-* */
-function onMouseWheel(event) {
-    //Normalize event wheel delta
-    var delta = event.originalEvent.wheelDelta / 30 || -event.originalEvent.detail;
-
-    //If the user scrolled up, it goes to previous slide, otherwise - to next slide
-    if (delta < -1) {
-        goToNextSlide();
-    }
-    else if (delta > 1) {
-        goToPrevSlide();
-    }
-
-    event.preventDefault();
-}
-
-/*
-*   If there's a previous slide, slide to it
-* */
-function goToPrevSlide() {
-    if ($currentSlide.prev().length) {
-        goToSlide($currentSlide.prev());
-    }
-}
-
-/*
-*   If there's a next slide, slide to it
-* */
-function goToNextSlide() {
-    if ($currentSlide.next().length) {
-        goToSlide($currentSlide.next());
-    }
-}
-
-/*
-*   Actual transition between slides
-* */
-function goToSlide($slide) {
-    //If the slides are not changing and there's such a slide
-    if (!isAnimating && $slide.length) {
-        //setting animating flag to true
-        isAnimating = true;
-        $currentSlide = $slide;
-
-        //Sliding to current slide
-        TweenLite.to($slidesContainer, 1, { scrollTo: { y: pageHeight * $currentSlide.index() }, onComplete: onSlideChangeEnd, onCompleteScope: this });
-
-        //Animating menu items
-        TweenLite.to($navButtons.filter(".active"), 0.5, { className: "-=active" });
-
-        TweenLite.to($navButtons.filter("[href=#" + $currentSlide.attr("id") + "]"), 0.5, { className: "+=active" });
-
-    }
-}
-
-/*
-*   Once the sliding is finished, we need to restore "isAnimating" flag.
-*   You can also do other things in this function, such as changing page title
-* */
-function onSlideChangeEnd() {
-    isAnimating = false;
-}
-
-/*
-*   When user resize it's browser we need to know the new height, so we can properly align the current slide
-* */
-function onResize(event) {
-
-    //This will give us the new height of the window
-    var newPageHeight = $window.innerHeight();
-
-    /*
-    *   If the new height is different from the old height ( the browser is resized vertically ), the slides are resized
-    * */
-    if (pageHeight !== newPageHeight) {
-        pageHeight = newPageHeight;
-
-        //This can be done via CSS only, but fails into some old browsers, so I prefer to set height via JS
-        TweenLite.set([$slidesContainer, $slides], { height: pageHeight + "px" });
-
-        //The current slide should be always on the top
-        TweenLite.set($slidesContainer, { scrollTo: { y: pageHeight * $currentSlide.index() } });
-    }
-
-}
